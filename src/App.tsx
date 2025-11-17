d3061e15596198dcbe1d8cc99f7d4d29af671e09
Gregory Dumas <gregorydumas@Gregorys-MacBook-Air.local>
2025-11-17 15:29:34 -0600

    Fix GS image address

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const serviceAreas = [
   {
     name: "Gulf Shores",
     blurb: "Sugar-sand beaches • Short-term rentals.",
-    img: "hhttps://images.unsplash.com/photo-1704291684136-7e902416cfb2?q=80&w=2350&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img: "https://images.unsplash.com/photo-1704291684136-7e902416cfb2?q=80&w=2350&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Gulf Shores beach shoreline",
   },
   {
